389d21f0246dc91ec9f0d00430fe9ee765c07991
Adam Alzuhairi <adam_alzu@hotmail.com>
2021-07-18 19:48:11 +0200

    Creation of project

---
+++ cmake-build-debug/CMakeFiles/autoclicker_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/autoclicker_project.dir/main.cpp.obj"
+  "autoclicker_project.exe"
+  "autoclicker_project.exe.manifest"
+  "autoclicker_project.lib"
+  "autoclicker_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/autoclicker_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
