34165bcc60474ba6298806514c79c40473ad7584
Adam Alzuhairi <adam_alzu@hotmail.com>
2021-10-26 16:12:57 +0200

    bort med text

--- main.cpp
+++ main.cpp
@@ -15,19 +15,10 @@ void mouseClick() {
     SendInput(3, inputs, sizeof(INPUT));
 }
 
-
 int main() {
     int milliSecond;
     const int minimumSeconds = 50;
     std::cout << "För att använda programmet så skall du ange millisekunder, sedan skall du peka på vart du vill autoclicka sedan trycka på F3 knappen i tangetbordet. " << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "För att pausa programmet ska du trycka på END knappen på tangetbordet." << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "För att minska användningen av datorns CPU och unvika överhettning så måste du sätta paustid mellan klickarna i millisekunder, minimum 50ms" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "Däremot, desto mindre millisekunder, desto snabbare klicks. Du måste dock vara försiktig med hastigheten för att undvika bli detectad och bannad. " << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "Så testa allt från en halvsekund (500ms) till mindre (50ms till 400ms)" << std::endl;
 
     std::cout << "" << std::endl;
     std::cout << "Ange millisekunder: ";
@@ -47,6 +38,7 @@ int main() {
             std::cout << "Programmet är pausat" << std::endl;
             goto execution;
         }
+
         }
     }
 
